375775afc21ddd8ca226ef7363f2c1dc813e33b6
Daniel Iovescu <iovescu.dani@gmail.com>
2024-01-09 12:08:38 +0200

    added users controller and mock compose for testing

--- auth/src/main/java/com/roddevv/controllers/AuthController.java
+++ auth/src/main/java/com/roddevv/controllers/AuthController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @RestController
 @RequestMapping("auth")
 public class AuthController {
