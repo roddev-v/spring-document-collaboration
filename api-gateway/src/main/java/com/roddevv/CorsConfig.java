951d25061fe0bd3edc662538b208291f331bade9
Daniel Iovescu <iovescu.dani@gmail.com>
2024-01-14 22:31:55 +0200

    stuff

--- api-gateway/src/main/java/com/roddevv/CorsConfig.java
+++ api-gateway/src/main/java/com/roddevv/CorsConfig.java
@@ -1,30 +1,36 @@
 package com.roddevv;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public UrlBasedCorsConfigurationSource corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOriginPatterns(Collections.singletonList("*"));
+    public CorsWebFilter corsWebFilter() {
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Collections.singletonList(CorsConfiguration.ALL));
+        corsConfig.addAllowedMethod(CorsConfiguration.ALL);
+        corsConfig.addAllowedHeader(CorsConfiguration.ALL);
         corsConfig.setMaxAge(3600L);
-        corsConfig.addAllowedMethod("*");
-        corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(true);
 
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/auth/**", corsConfig);
         source.registerCorsConfiguration("/notifications/**", corsConfig);
         source.registerCorsConfiguration("/document/**", corsConfig);
-        return source;
+        source.registerCorsConfiguration("/content/**", corsConfig);
+        source.registerCorsConfiguration("/rtc/**", corsConfig);
+        return new CorsWebFilter(source);
     }
 }
