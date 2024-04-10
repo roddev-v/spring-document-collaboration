33bbb3dc635fb166aad60a5e770173f31532a612
Daniel Iovescu <iovescu.dani@gmail.com>
2024-04-10 23:15:40 +0300

    Added debug logger on RTC

--- real-time/src/main/java/com/roddevv/controllers/RealTimeController.java
+++ real-time/src/main/java/com/roddevv/controllers/RealTimeController.java
@@ -2,7 +2,10 @@ package com.roddevv.controllers;
 
 import com.roddevv.dto.ClientEventDto;
 import com.roddevv.dto.EditingEventDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -12,14 +15,20 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class RealTimeController {
 
+    final Logger logger = LoggerFactory.getLogger(RealTimeController.class);
+
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
     @Autowired
     private KafkaTemplate<String, EditingEventDto> documentEditingEvents;
 
+    @Value("${HOSTNAME}")
+    private String hostname;
+
     @MessageMapping("/events/{documentId}")
     public void sendMessage(@DestinationVariable String documentId, ClientEventDto event) {
+        logger.debug(String.format("Broadcasting document %s on host %s", documentId, hostname));
         messagingTemplate.convertAndSend("/events/updates/" + documentId, event);
 
         final EditingEventDto dto = EditingEventDto.builder()
