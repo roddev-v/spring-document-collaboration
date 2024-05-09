0e63c9443ee43604546a091effd2eeb0d8530d53
Daniel Iovescu <iovescu.dani@gmail.com>
2024-05-10 01:27:46 +0300

    All args added

--- real-time/src/main/java/com/roddevv/dto/EventBroadcastDto.java
+++ real-time/src/main/java/com/roddevv/dto/EventBroadcastDto.java
@@ -1,10 +1,12 @@
 package com.roddevv.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class EventBroadcastDto {
     private String id;
     private ClientEventDto event;
