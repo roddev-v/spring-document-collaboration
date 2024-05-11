a2446c46110d475f4ef4f815db81a27e44f74a90
Daniel Iovescu <iovescu.dani@gmail.com>
2024-05-11 22:12:43 +0300

    Added default constructors on objects

--- real-time/src/main/java/com/roddevv/dto/ClientEventDto.java
+++ real-time/src/main/java/com/roddevv/dto/ClientEventDto.java
@@ -1,10 +1,14 @@
 package com.roddevv.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientEventDto {
     private String type;
     private List<Object> state;
--- real-time/src/main/java/com/roddevv/dto/EventBroadcastDto.java
+++ real-time/src/main/java/com/roddevv/dto/EventBroadcastDto.java
@@ -3,10 +3,12 @@ package com.roddevv.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class EventBroadcastDto {
     private String id;
     private ClientEventDto event;
