bc5a451478e516d84d4897276bd3978882e6bfb8
Daniel Iovescu <iovescu.dani@gmail.com>
2024-05-25 14:46:45 +0300

    com.roddevv.dto.EditingEventDto public constructor

--- real-time/src/main/java/com/roddevv/dto/EditingEventDto.java
+++ real-time/src/main/java/com/roddevv/dto/EditingEventDto.java
@@ -1,10 +1,14 @@
 package com.roddevv.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EditingEventDto {
     private String documentId;
     private String content;
