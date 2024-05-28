b8589f387d510209d997fd36e46447b9ef2aea5a
Daniel Iovescu <iovescu.dani@gmail.com>
2024-05-28 12:22:18 +0300

    notifications changes

--- notifications/src/main/java/com/roddevv/dto/NotificationDto.java
+++ notifications/src/main/java/com/roddevv/dto/NotificationDto.java
@@ -1,12 +1,39 @@
 package com.roddevv.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ImportRuntimeHints(NotificationDto.NotificationDtoRuntimeHints.class)
 public class NotificationDto {
-    private Long senderId;
-    private String senderEmail;
-    private String senderNickname;
-    private Long recipientId;
-    private String type;
+    public Long senderId;
+    public String senderEmail;
+    public String senderNickname;
+    public Long recipientId;
+    public String type;
+
+    static class NotificationDtoRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.reflection().registerType(NotificationDto.class, hint -> {
+                hint.withMembers(
+                        MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
+                        MemberCategory.INVOKE_PUBLIC_METHODS,
+                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                        MemberCategory.INVOKE_DECLARED_METHODS,
+                        MemberCategory.PUBLIC_FIELDS,
+                        MemberCategory.DECLARED_FIELDS
+                );
+            });
+        }
+    }
 }
