672b7610afa363852cdaf29bff067ec67d13c6d6
Daniel Iovescu <iovescu.dani@gmail.com>
2024-05-28 12:04:19 +0300

    WIP

--- document-content-handler/src/main/java/com/roddevv/config/EditingEventConsumer.java
+++ document-content-handler/src/main/java/com/roddevv/config/EditingEventConsumer.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
