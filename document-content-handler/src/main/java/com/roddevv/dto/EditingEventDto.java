3723d896ac6d494a8544a0290d1d2592c58266b9
Daniel Iovescu <iovescu.dani@gmail.com>
2024-05-28 11:52:05 +0300

    added relfection config

--- document-content-handler/src/main/java/com/roddevv/dto/EditingEventDto.java
+++ document-content-handler/src/main/java/com/roddevv/dto/EditingEventDto.java
@@ -1,6 +1,7 @@
 package com.roddevv.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.aot.hint.MemberCategory;
@@ -9,6 +10,7 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ImportRuntimeHints(EditingEventDto.EditingEventDtoRuntimeHints.class)
