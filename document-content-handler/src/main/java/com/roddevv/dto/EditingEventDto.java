3ec0e49a122a102f1eb1b1cafa9d888f5bfee9c2
Daniel Iovescu <iovescu.dani@gmail.com>
2024-05-28 11:27:57 +0300

    Added hints upon editing event dto

--- document-content-handler/src/main/java/com/roddevv/dto/EditingEventDto.java
+++ document-content-handler/src/main/java/com/roddevv/dto/EditingEventDto.java
@@ -1,10 +1,35 @@
 package com.roddevv.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ImportRuntimeHints(EditingEventDto.EditingEventDtoRuntimeHints.class)
 public class EditingEventDto {
-    private String documentId;
-    private String content;
-    private String eventType;
+    public String documentId;
+    public String content;
+    public String eventType;
+
+    static class EditingEventDtoRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.reflection().registerType(EditingEventDto.class, hint -> {
+                hint.withMembers(
+                        MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
+                        MemberCategory.INVOKE_PUBLIC_METHODS,
+                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                        MemberCategory.INVOKE_DECLARED_METHODS,
+                        MemberCategory.PUBLIC_FIELDS,
+                        MemberCategory.DECLARED_CLASSES
+                );
+            });
+        }
+    }
 }
