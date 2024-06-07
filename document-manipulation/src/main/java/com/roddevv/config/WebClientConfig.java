90537ba7bc5c99eb4e3b379bb7643c8855707699
Daniel Iovescu <iovescu.dani@gmail.com>
2024-06-07 20:24:18 +0300

    Readded package

--- document-manipulation/src/main/java/com/roddevv/config/WebClientConfig.java
+++ document-manipulation/src/main/java/com/roddevv/config/WebClientConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder();
     }
